5a7334248861445cf90a51d7ea5e207f5a7e18ba
Matt Brody <mattlbrody@gmail.com>
2014-06-13 22:01:43 -0700

    first commit

---
+++ contacted.php
@@ -0,0 +1,177 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Codify Academy</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <link rel="icon" type="image/ico" href="http://codifyacademy.com/favicon.ico">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    
+  <!-- Google Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Ruda:400,900,700' rel='stylesheet' type='text/css'>  
+  
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="css/font-awesome.css">
+  <!-- Bootstrap -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  <!-- Template Styles -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/colors/turquoise.css">    
+  <!-- Layer Slider -->
+  <link rel="stylesheet" href="layerslider/css/layerslider.css" type="text/css">
+
+  
+  <!-- Support for HTML5 -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Enable media queries on older browsers -->
+  <!--[if lt IE 9]>
+    <script src="assets/js/respond.min.js"></script>
+  <![endif]-->
+  <script src="js/phone.js"></script>
+  <script src="js/modernizr.js"></script>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46682051-2', 'codifyacademy.com');
+  ga('send', 'pageview');
+
+  </script>
+  </head>
+<!--   <div class="topbar clearfix">
+    <div class="container">
+    <div class="col-lg-12 text-right">
+              <div class="social_buttons">
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Dribbble"><i class="fa fa-dribbble"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="RSS"><i class="fa fa-rss"></i></a>
+              </div>
+    </div>
+    </div>
+  </div> -->
+
+  <header class="header">
+    <div class="container">
+        <div class="site-header clearfix">
+              <div class="col-lg-3 col-md-3 col-sm-12 title-area">
+                  <div class="site-title" id="title">
+                      <a href="index.php" title="">
+                          <h4>Codify <span>Academy</span></h4>  
+                      </a>
+                  </div>
+              </div><!-- title area -->
+              <div class="col-lg-9 col-md-12 col-sm-12">
+                 <div id="nav" class="right">
+                      <div class="container clearfix navbar-right">
+                      <ul id="jetmenu" class="jetmenu blue">
+                          <li><a href="index.php">Home</a>
+                          </li>
+                          <li><a href="thecourse.php">The Course</a> 
+                          </li>
+                          <li><a href="apply.php">Apply</a>
+                          </li>
+                          <li><a href="faq.php">FAQ</a>
+                          </li>
+                          <li class="active"><a href="contact.php">Contact</a>
+                              
+                          </li>
+                      </ul>
+                      </div>
+                  </div>  
+              </div>
+          </div><!-- site header -->
+    </div><!-- end container -->
+  </header><!-- end header -->
+<body>
+  <div class="jumbotron jumbotron-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="h1">
+                    Contact us <small>Feel free to reach out</small></h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+	<h1>Your Message Has Been Sent!<h1>
+</div>
+<div class="container contactfooter">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="well well-sm">
+                <form method="post" action="mail.php">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">
+                                Name</label>
+                            <input name="contactname" type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                Email Address</label>
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input name="contactemail" type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">
+                                Message</label>
+                            <textarea name="contactmessage" id="message" class="form-control" rows="9" cols="25" required="required"
+                                placeholder="Message"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <button type="submit" name="submit" class="btn btn-primary pull-right" id="btnContactUs">
+                            Send Message</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <form>
+            <legend><span class="glyphicon glyphicon-globe"></span> Our office</legend>
+            <address>
+                <strong>Codify Academy</strong><br>
+                55 Main St<br>
+                Yonkers, NY 10701<br>
+                <abbr title="Phone">
+                    P:</abbr>
+                (571) 421-7308
+            </address>
+            <address>
+                <strong>Sam Brody</strong><br>
+                <a href="mailto:sambrody24@gmail.com">sambrody24@gmail.com</a>
+            </address>
+            <address>
+                <strong>Matt Brody</strong><br>
+                <a href="mailto:sambrody24@gmail.com">mattlbrody@gmail.com</a>
+            </address>
+            <address>
+                <strong>Chris Brody</strong><br>
+                <a href="mailto:sambrody24@gmail.com">chrisbrody87@gmail.com</a>
+            </address>
+            </form>
+        </div>
+    </div>
+</div>
+
+  <?php include('footer.php');?>
+
+</body>
+</html>
---
+++ faq.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Codify Academy</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <link rel="icon" type="image/ico" href="http://codifyacademy.com/favicon.ico">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    
+  <!-- Google Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Ruda:400,900,700' rel='stylesheet' type='text/css'>  
+  
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="css/font-awesome.css">
+  <!-- Bootstrap -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  <!-- Template Styles -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/colors/turquoise.css">    
+  <!-- Layer Slider -->
+  <link rel="stylesheet" href="layerslider/css/layerslider.css" type="text/css">
+
+  
+  <!-- Support for HTML5 -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Enable media queries on older browsers -->
+  <!--[if lt IE 9]>
+    <script src="assets/js/respond.min.js"></script>
+  <![endif]-->
+  <script src="js/phone.js"></script>
+  <script src="js/modernizr.js"></script>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46682051-2', 'codifyacademy.com');
+  ga('send', 'pageview');
+
+  </script>
+  </head>
+<!--   <div class="topbar clearfix">
+    <div class="container">
+    <div class="col-lg-12 text-right">
+              <div class="social_buttons">
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Dribbble"><i class="fa fa-dribbble"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="RSS"><i class="fa fa-rss"></i></a>
+              </div>
+    </div>
+    </div>
+  </div> -->
+
+  <header class="header">
+    <div class="container">
+        <div class="site-header clearfix">
+              <div class="col-lg-3 col-md-3 col-sm-12 title-area">
+                  <div class="site-title" id="title">
+                      <a href="index.php" title="">
+                          <h4>Codify <span>Academy</span></h4>  
+                      </a>
+                  </div>
+              </div><!-- title area -->
+              <div class="col-lg-9 col-md-12 col-sm-12">
+                 <div id="nav" class="right">
+                      <div class="container clearfix navbar-right">
+                      <ul id="jetmenu" class="jetmenu blue">
+                          <li><a href="index.php">Home</a>
+                          </li>
+                          <li><a href="thecourse.php">The Course</a> 
+                          </li>
+                          <li><a href="apply.php">Apply</a>
+                          </li>
+                          <li class="active"><a href="faq.php">FAQ</a>
+                          </li>
+                          <li><a href="contact.php">Contact</a>
+                              
+                          </li>
+                      </ul>
+                      </div>
+                  </div>  
+              </div>
+          </div><!-- site header -->
+    </div><!-- end container -->
+  </header><!-- end header -->
+<body>
+  <div class="jumbotron jumbotron-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="h1 h1course">Frequently Asked Questions</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class=" row rowwidth">
+  <div class="col-lg-2 col-lg-offset-1">
+      <div class="circleimage">
+        <img src="demos/chris.jpg" class="img-circle" style="height:116px" "width:116px"/>
+      </div>
+  </div>
+  <div class="col-lg-9">
+    <h2>Have any other questions?<h2>
+    <h3>Call Chris, (703) 887-9685</h3>
+  </div>
+</div>
+<br>
+<hr>
+
+<div class="container section-grey">
+    <div class="row rowwidth">
+      <div class="col-lg-12 columns font-size-150-percent">
+
+        <h4 class="text-center"><strong>What Codify Academy Looks for in Applicants</strong></h4>
+
+        <br>
+
+        <p><strong>What are the technical qualifications?</strong></p>
+
+        <p>There are no technical qualifications, only a drive to succeed is required. There's not doubt that knowing some basic coding concepts will help you finish the work faster. But our course starts from the very beginning. As long as you know what a computer is, you're qualified to take our course. </p>
+
+        <br>
+
+        <p><strong>What are the non-technical qualifications?</strong></p>
+
+        <p> • Passionate, intelligent learners
+            <br>
+            • Clear, empathic communicators
+            <br>
+            • Dedicated and curious
+        </p>
+
+        <br>
+
+        <p><strong>Who is the typical Codify Academy applicant?</strong></p>
+
+        <p> There isn't really a "typical" Codify Academy applicant. Our students come from a variety of different backgrounds. Some have absolutely no background in coding and want to get web developer training to apply for very high paying jobs. Some come from adjacent fields, like design or IT, and have been tangentially working with coding for a while. Some have been learning programming on their own, and wanted to get the complete skill set they can start a dev career.
+        </p>
+
+        <br>
+
+        <h4 class="text-center"><strong>About the Admissions Process</strong></h4>
+
+        <br>
+
+        <p><strong>How can I begin?</strong></p>
+
+        All you have to do is sign up (link) and choose which class time and date works for you.
+        </p>
+
+        <br>
+
+        <p><strong>What will happen after I complete an application?</strong></p>
+
+        <p>You'll receive an email from us with links on where to start your training, so you come to the first class ready to build in HTML.
+        </p>
+
+        <br>
+        <h4 class="text-center"><strong>When to Apply</strong></h4>
+
+        <br>
+
+        <p><strong>When does the next class start?</strong></p>
+
+        <p>A new class starts about every three weeks. Our July 2014 start dates are:</p>
+
+        <p>
+          • July 6
+          <br>
+          • July 6
+          <br>
+          • July 26
+          <br>
+          • July 27
+        </p>
+
+        <br>
+
+        <p><strong>When should I apply to get into a particular class?</strong></p>
+
+        <p> You should apply as soon as you feel ready. If you think it may be too early, it’s (almost) never too early to apply. If there's room in the class, you may put down the deposit on a specific class.</p>
+        <p>If you think it may be too late: There is no deadline for applying to a given class. However, if you are aiming for a specific start date, we recommend submitting your application as soon as possible. The class fills up whenever we admit enough students.</p>
+
+        <br>
+
+        <p><strong>What if I am accepted into Codify Academy and need to change my start date?</strong></p>
+
+        <p> We try to help our admitted applicants make the best choice of start date at the time of their admissions decision. However, if something comes up and you need to change to a later date, we would do our best to accommodate you. We would ask that you let us know as soon as possible if it starts looking like you need to switch, both to make sure there will be room for you in the later class and so that someone else would be able to take advantage of your vacant seat. It should be noted that we handle requests to switch start dates on a case-by-case basis and cannot provide any guarantees.
+        </p>
+
+        <br>
+
+        <h4 class="text-center"><strong>Tuition</strong></h4>
+
+        <br>
+
+        <p><strong>How much does tuition cost?</strong></p>
+
+        <p> Tuition is more affordable than a single night class at a univeristy: $1,999 for <a href="thecourse.php">12 weeks, 3 hours of in-classroom instruction on a weekend day, 7-12 hours of courswork outside of class</a>.
+        </p>
+
+        <br>
+
+        <p><strong>Are there loan options? How does the payment plan work?</strong></p>
+
+        <p>In terms of student loans, we have not been able to jump through the necessary bureaucratic hoops to qualify for federally-subsidized education programs, etc.
+        </p>
+
+        <p>Our payment plan is for students who would otherwise be unable to attend. It is decided on a case-by-case basis - after we have a complete picture of you as an applicant, as well as your individual financial situation - to delay paying up to half of the cost of tuition after the first month of the class.
+        </p>
+
+        <br>
+
+        <p><strong>How do I find out more?</strong></p>
+
+        <p> You can send an email to <a href="mailto:chrisbrody87@gmail.com">chrisbrody87@gmail.com</a> if you have a question that is not answered on this web page.
+        </p>
+        <br>
+        <br>
+    </div>
+  </div>
+</div>
+
+  <?php include('footer.php');?>
+    
+</body>
+</html>
